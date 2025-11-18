28408eb025b39193c4ba4e9bdc81fa5a0cfb7de6
leeeunji <ayozz6742@naver.com>
2025-11-18 22:11:02 +0900

    n

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects(){
     
     async function loadJsonData() {
         try {
-            const res = await fetch("/data/projects.json");
+            const res = await fetch(import.meta.env.BASE_URL + "data/projects.json");
             const data = await res.json();
             setProjects(data);
         } catch(error){
